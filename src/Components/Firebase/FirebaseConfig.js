57d644d79b402f49a2cbada6df451f06ced27989
Md. Shoaib Akhter Shifat <75001645+Bad-Luck@users.noreply.github.com>
2021-08-24 11:53:46 +0600

    gitignore updated

--- src/Components/Firebase/FirebaseConfig.js
+++ src/Components/Firebase/FirebaseConfig.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA6qFZ5cTMDLr_CiV8Q4ogz69jopg5jwOA",
-  authDomain: "travel-agency-a7ee7.firebaseapp.com",
-  projectId: "travel-agency-a7ee7",
-  storageBucket: "travel-agency-a7ee7.appspot.com",
-  messagingSenderId: "129271102117",
-  appId: "1:129271102117:web:ca2e8c8d8ab72b4c35eb4c",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 if (!firebase.apps.length) {
